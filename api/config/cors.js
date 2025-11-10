f333ac1e482be2fa95ecac2c07a410e365737042
homow <homowsh897@gmail.com>
2025-11-11 00:21:09 +0330

    udpate logic in cors

--- api/config/cors.js
+++ api/config/cors.js
@@ -1,12 +1,26 @@
 export default function applyCors(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	const allowedOrigins = [
+		"https://home-nest-admin.vercel.app",
+		"https://home-nest.vercel.app",
+		"http://localhost:5173",
+		"http://localhost:5174",
+		"http://localhost:4173/",
+	];
 
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return true;
-  }
+	const origin = req.headers.origin;
 
-  return false;
+	if (allowedOrigins.includes(origin) ) {
+		res.setHeader('Access-Control-Allow-Origin', origin);
+	}
+	
+	req.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+	if (req.method === 'OPTIONS') {
+		res.status(200).end();
+		return true;
+	}
+
+	return false;
 }
