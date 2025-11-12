731efb6ce725189f589846a1787e2f3c5f534078
homow <homowsh897@gmail.com>
2025-11-12 04:07:12 +0330

    completed supabaseServer to type sttic

--- api/config/supabaseServer.ts
+++ api/config/supabaseServer.ts
@@ -1,11 +1,15 @@
+import type {SupabaseClient, SupabaseClientOptions} from "@supabase/supabase-js";
 import {createClient} from "@supabase/supabase-js";
 
-const supabaseServer = (opt = {}) => {
-    return createClient(
-        process.env.SUPABASE_URL,
-        process.env.SUPABASE_SERVICE_ROLE_KEY,
-        {...opt}
-    )
-}
+type UnType = any;
 
-export default supabaseServer;
+export default function supabaseServer(opt: SupabaseClientOptions<UnType> = {}): SupabaseClient {
+    const url: string | undefined = process.env.SUPABASE_URL;
+    const key: string | undefined = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+    if (!url || !key) {
+        throw new Error('Missing SUPABASE_URL or SUPABASE_SERVICE_ROLE_KEY in environment variables');
+    }
+
+    return createClient(url, key, opt);
+};
