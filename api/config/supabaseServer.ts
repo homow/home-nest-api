8dba2d583d30eed5dd12ac2fdac741a510728d2d
homow <homowsh897@gmail.com>
2025-11-12 04:08:18 +0330

    update supabaseServer type

--- api/config/supabaseServer.ts
+++ api/config/supabaseServer.ts
@@ -1,15 +1,15 @@
-import type {SupabaseClient, SupabaseClientOptions} from "@supabase/supabase-js";
-import {createClient} from "@supabase/supabase-js";
+import {createClient} from '@supabase/supabase-js';
+import type {SupabaseClient, SupabaseClientOptions} from '@supabase/supabase-js';
 
-type UnType = any;
+type UntypedDb = any;
 
-export default function supabaseServer(opt: SupabaseClientOptions<UnType> = {}): SupabaseClient {
-    const url: string | undefined = process.env.SUPABASE_URL;
-    const key: string | undefined = process.env.SUPABASE_SERVICE_ROLE_KEY;
+export default function supabaseServer(opt: SupabaseClientOptions<UntypedDb> = {}): SupabaseClient<UntypedDb> {
+    const url = process.env.SUPABASE_URL;
+    const key = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
     if (!url || !key) {
         throw new Error('Missing SUPABASE_URL or SUPABASE_SERVICE_ROLE_KEY in environment variables');
     }
 
-    return createClient(url, key, opt);
-};
+    return createClient<UntypedDb>(url, key, opt);
+}
